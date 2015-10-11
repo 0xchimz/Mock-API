b0e8670e0ed95f88acb68c84df185e653c1733f8
ChinCluBi <chincub@gmail.com>
2015-10-11 18:39:20 +0700

    handle login post and change to promise

--- app/lib/mysql.js
+++ app/lib/mysql.js
@@ -1,10 +1,11 @@
 var mysql = require('mysql')
+var q = require('q')
 
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
-  database: 'freshycamp'
+  database: 'Mock-API'
 })
 
 connection.connect(function (error) {
@@ -14,3 +15,48 @@ connection.connect(function (error) {
     console.log('[db] Error while connecting database ...')
   }
 })
+
+module.exports = {
+  register: register,
+  findUser: findUser,
+  login: login
+}
+
+function register (data, callback) {
+  var deferred = q.defer()
+  connection.query('INSERT INTO user SET ?', data, function (error, response) {
+    console.log('INSERT TO DB')
+    if (!error) {
+      deferred.resolve(response)
+    } else {
+      deferred.reject(error)
+    }
+  })
+  return deferred.promise
+}
+
+function findUser (data) {
+  var deferred = q.defer()
+  var strQuery = "SELECT * FROM user WHERE email = '" + data.email + "'"
+  connection.query(strQuery, function (error, rows) {
+    if (!error) {
+      deferred.resolve(rows)
+    } else {
+      deferred.reject(error)
+    }
+  })
+  return deferred.promise
+}
+
+function login (data) {
+  var deferred = q.defer()
+  var strQuery = "SELECT * FROM user WHERE email = '" + data.email + "' AND password = '" + data.password + "' LIMIT 1"
+  connection.query(strQuery, function (error, rows) {
+    if (!error) {
+      deferred.resolve(rows)
+    } else {
+      deferred.reject(error)
+    }
+  })
+  return deferred.promise
+}
